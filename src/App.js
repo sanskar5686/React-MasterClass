d7a7efed27da1dcf8b5085bd7d74c9465bb87b9a
sanskar5686 <kumarsanskar5686@gmail.com>
2024-06-07 20:22:44 +0530

    Initial commit - Day 1

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './index.css'
+
+function App(){
+  return(<div className="App-header">
+    Hello
+  </div>)
+}
+export default App;
---
+++ src/Helper.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Helper(props) {
+  return (
+    <>
+    <img
+    src='https://i.imgur.com/MK3eW3As.jpg'
+    alt='Not loading!'
+    />
+    <div>{props.title}</div>
+    </>
+  )
+}
+
+export default Helper
